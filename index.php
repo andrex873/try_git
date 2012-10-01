12e5159fd8c66486c33bb17015bfdb1678b6467b
Andrés Méndez Juanias <amj.desarrollo@gmail.com>
2012-10-01 14:00:26 -0500

    Nuevo texto en conflicto.

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 	<h1>try git amendez
 	</h1>
 	<div>este cambio se hace en el propio github</div>
-	<footer>This is the footer of the app and it'll generate a conflict with the local files. :)</footer>
+	<div>
+		texto en conflicto que contien mas de 1 linea.
+	</div>	
+	<footer>This is the footer of the app and it'll generate a conflict with the local files. :)</footer>	
 </body>
 </html>
