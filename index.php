4824417eeb2d97d45a4f1419f1f11a59899f0e9a
Andrés Méndez Juanias <amj.desarrollo@gmail.com>
2012-10-01 12:14:18 -0500

    Se agrega linea para generar conflicto local.

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 	<h1>try git amendez
 	</h1>
 	<div>este cambio se hace en el propio github</div>
+	<footer>This is the footer of the app and it'll generate a conflict with the local files. :)</footer>
 </body>
 </html>
