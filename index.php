c97da753bc30bc51ed97bc75be576cdc03e1f001
amendez home <amj.desarrollo@gmail.com>
2012-11-03 21:44:00 -0500

    Se limpia el archivo index

--- index.php
+++ index.php
@@ -2,15 +2,9 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title>Master branch</title>
 </head>
 <body>
-	<h1>try git amendez
-	</h1>
-	<div>este cambio se hace en el propio github</div>
-	<div>
-		texto en conflicto que contien mas de 1 linea.
-	</div>	
-	<footer>This is the footer of the app and it'll generate a conflict with the local files. :)</footer>	
+	<h1>Titulo de la pagina</h1>
 </body>
 </html>
